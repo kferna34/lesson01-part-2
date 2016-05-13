e1813b557df46644ba068490d766a65150f5f97f
kferna34 <kferna34@my.centennialcollege.ca>
2016-05-13 10:43:16 -0400

    added info

---
+++ week01-02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+*author : Krytia Fernandez
+*student number : 300845885
+*date: may 13,2016
+*/
+namespace week01_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
